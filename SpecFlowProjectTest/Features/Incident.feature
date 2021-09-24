dc9dde9f21cd64d6ee6a0a2f48ce5a992cc5a4ef
yasharth.bhatt <yasharth.bhatt@gmail.com>
2021-09-24 13:50:14 +0530

    Update the incident file

---
+++ SpecFlowProjectTest/Features/Incident.feature
@@ -0,0 +1,46 @@
+﻿Feature: Incident
+	Simple calculator for adding two numbers
+
+@ValidUser
+Scenario: Validate the user authentication
+	Given  the ServiceNow user with ID "TestUser" and Password "Passw0rd"
+	When Validating the user authentication
+	Then the response code should be "OK"
+
+@InvalidUser
+Scenario: Validate the user authentication
+	Given  the ServiceNow user with ID "xyz" and Password "123456"
+	When Validating the user authentication
+	Then the response code should be "Unauthorized"
+
+@IncidentNumber,@ResponseCode
+Scenario: Retireve and Validate the incident specific field details with the given incident ID
+	Given User with ID "TestUser" and Password "Passw0rd" is authenticated for the ServiceNow Instance
+	When an incident id is shared for the user with number as "INC0000060"
+	Then the most recent incident  is as follows
+		| attribute    | value      |
+		| number       | INC0000060 |
+		| ResponseCode | 200        |
+
+@ChildIncidents
+Scenario: Retireve and Validate the child_incidents details with the given incident ID
+	Given  the user with ID "admin" is authenticated for the ServiceNow Instance
+	When a incident id is shared for the user with number as "INC0000060"
+	Then the most recent incident for user admin includes:
+		| child_incidents | 0             |
+	And the response code should be "200"
+
+@ChildIncidents
+#Increasing Maintainability 
+#Scenario outlines - Group all "And tan incident id is shared for the user with number X" statement into one scenario
+Scenario Outline: Retireve and Validate the child_incidents details with the given incident ID
+	Given User with ID "TestUser" and Password "Passw0rd" is authenticated for the ServiceNow Instance
+	And  an incident id is shared for the user with number as <incidentnumber> USD
+	Then response code should be <ResponseCode>
+	And child incidents should be <child_incidents>
+	
+	Examples:
+		| incidentnumber | ResponseCode | child_incidents |
+		| INC0000060 | 200   | 0           |
+		| INC0000061 | 200   | 0           |
+		| INC0000062 | 200   | 0           |
---
+++ SpecFlowProjectTest/Steps/Incident.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpecFlowTraining;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using RestSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace SpecFlowProjectTest.Steps
+{
+    [Binding]
+    public class CCPropertiesSteps
+    {
+        private Incident _inc;
+        IRestResponse response;
+        private string baseUrl = "https://dev98654.service-now.com";
+
+        [Given(@"User with ID (.*) and password (.*)")]
+        public void GivenUserWithIDAndPassword(string userId, String password)
+        {
+            RestClient client = new RestClient(url);
+            client.Authenticator = new HttpBasicAuthenticator(userName, password);
+        }
+
+        [When(@"Validating the user authentication")]
+        public void WhenValidatingTheUserAuthentication()
+        {
+            var request = new RestRequest("resource", Method.GET);
+            response = client.Execute(request);
+        }
+
+        [Then(@"Response code should be (.*)")]
+        public void ThenResponseCodeShouldBe(string responseCode)
+        {
+            Assert.AreEqual(response.StatusCode, responseCode);
+        }
+
+        [Given(@"User with ID (.*) and Password (.*) is authenticated for the ServiceNow Instance")]
+        public void GivenUserAndPasswordIsAuthenticated(string userId, String password)
+        {
+            RestClient client = new RestClient(url);
+            client.Authenticator = new HttpBasicAuthenticator(userName, password);
+            var request = new RestRequest("resource", Method.GET);
+            response = client.Execute(request);
+            Assert.AreEqual(response.StatusCode, responseCode);
+        }
+
+
+        [When(@"an incident id is shared for the user with number as (.*)")]
+        public void WhenAnIncidentIdIsSharedForTheuserWithTheNumber(string incidentNumber)
+        {
+            var request = new RestRequest("resource", Method.GET);
+            request.AddParameter("number", incidentNumber);
+            response = client.Execute(request);
+        }
+
+        [Then(@"the most recent incident  is as follows")]
+        public void ThenMostRecentIncidentIsAsFollws(Table table)
+        {
+            //---------Weekly Typed Conversion---------------
+            var number = table.Rows.First(row => row["attribute"] == "number")["value"];
+            var responseCode = table.Rows.First(row => row["attribute"] == "ResponseCode")["value"];
+
+            Assert.AreEqual(response.StatusCode, responseCode);
+            Assert.AreEqual(response.number, number);
+        }
+    }
+}
